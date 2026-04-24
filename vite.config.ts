5347f708aff125700893562e007ce5022190934f
Pedro Ferreira <plferreira4@gmail.com>
2026-04-24 21:03:34 +0200

    🐛: Fix server host issue.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    server: {
+        host: "127.0.0.1",
+        hmr: {
+            host: "127.0.0.1",
+        },
+    },
     test: {
         coverage: {
             provider: "v8",
